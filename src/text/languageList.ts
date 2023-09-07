98b5a290acb24e2c3b37af5d3d9bda52c790d30f
pacattac <123616150+pacattac@users.noreply.github.com>
2023-09-07 10:47:27 -0400

    Spanish
    Added support for Spanish language.

--- src/text/languageList.ts
+++ src/text/languageList.ts
@@ -1,6 +1,7 @@
 import en_US from './en-US.json' assert { type: 'json' };
 import nl_NL from './nl.json' assert { type: 'json' };
 import de from './de.json' assert { type: 'json' };
+import es from './es.json' assert { type: 'json' };
 import type { LangMap } from '../types/types.js';
 
 export type LangKey = keyof typeof en_US;
@@ -9,5 +10,6 @@ const languages: LangMap = {
     'en-US': en_US,
     'nl': nl_NL,
     'de': de,
+	'es': es
 };
 export default languages;
